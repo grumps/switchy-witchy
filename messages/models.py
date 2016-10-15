926046bb3d98a42d588ea8353bf70da80ab643e3
grumps <max@ofmax.li>
2016-10-14 22:51:16 -0700

    add mypy-lang, add typing

--- messages/models.py
+++ messages/models.py
@@ -2,43 +2,47 @@ __author__ = 'Maxwell J. Resnick'
 __docformat__ = 'reStructuredText'
 import json
 import datetime
+from typing import Dict
 
-class Message:
+
+class BaseMessage:
     """
     Represents a message.
-    
+
     :param data
     :param sender
-    .. py:attribute:: _initial base message obj, internal use
+    .. py:attribute:: _initial message obj before encode, internal use
     """
-    _initial = None
-
-    def __init__(self, data=None, sender=None):
+    
+    _initial = {}  # type: Dict[str, str]
+    
+    def __init__(self, data: str = None, sender: str = None) -> None:
         self.is_valid(data, sender)
-        self._initial.update({'create_timestamp': self.create_timestamp})
+        self._initial.update({'create_timestamp': self.create_timestamp()})
 
-    def is_valid(self, data=None, sender=None):
+    def is_valid(self, data: str = None, sender: str = None) -> bool:
         """checks for a valid message, sets .initial
         :returns: boolean
         :rtype: bool
         """
+        # TODO
         assert sender is not None
         assert data is not None
-        self._initial = {
-                'sender': sender,
-                'data': data}
+        self.sender = sender
+        self.data = data
         return True
-    
-    def create_timestamp(self):
+
+    def create_timestamp(self) -> str:
         """creation timestamp, utc
         :return: datetime.datetime.utcnow()
         :rtype: object
         """
-        return datetime.datetime.utcnow()
+        return datetime.datetime.utcnow().isoformat()
 
-    def encode(self):
+
+    def encode(self) -> str:
         """JSON encodes .initial
         :returns: json.dump
-        :rtype: str
+        :rtype: bytes
         """
-        return json.dump(initial) 
+        return json.dumps(self._initial)
